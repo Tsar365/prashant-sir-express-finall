cbd2bf7c98bd24e46144d13f1f7884bb09206a99
Tsar365 <ehasanmd365@gmail.com>
2026-02-24 23:25:36 +0600

    Delete folder7 directory

--- folder7/bs-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  proxy: "http://localhost:3000", // your Express server
-  files: ["views/**/*.html", "public/css/output.css"],
-  open: false,
-  notify: false
-};
--- folder7/routes/submitted.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRouter = express.Router();
-const submittedController = require("../controller/submitted");
-
-userRouter.post("/submitted", submittedController.submitted);
-
-exports.registerHomes = submittedController.registerHomes;
-exports.userRouter = userRouter;
